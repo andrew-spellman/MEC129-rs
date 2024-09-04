47f992ea99e15b97f211c33b08e7f6454c3dad14
Andrew Spellman <spellmanandrew@protonmail.com>
2024-09-04 11:21:56 -0500

    Impled get hv feed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![no_std]
 #![no_main]
 
-use embedded_hal::digital::{OutputPin, StatefulOutputPin};
-// use panic_halt as _;
+use embedded_hal::{adc::OneShot, digital::v2::OutputPin};
 
 use rp2040_hal::{
+    adc::{Adc, AdcPin},
     clocks::init_clocks_and_plls,
-    gpio::{bank0, FunctionSio, Pin, PinState, Pins, PullDown, SioOutput},
+    gpio::{PinState, Pins},
     pac::Peripherals,
     usb::UsbBus,
     Sio, Timer, Watchdog,
@@ -15,7 +15,6 @@ use rp2040_hal::{
 use usb_device::{class_prelude::*, prelude::*};
 use usbd_serial::{embedded_io::Write, SerialPort};
 
-use core::fmt::Write as OtherWrite;
 use core::panic::PanicInfo;
 use heapless::String;
 
@@ -29,19 +28,44 @@ pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
 /// if your board has a different frequency
 const EXTERN_FREQ_HZ: u32 = 12_000_000u32;
 
-static mut TIMER: Option<Timer> = None;
-static mut LED_RED: Option<Pin<bank0::Gpio25, FunctionSio<SioOutput>, PullDown>> = None;
-static mut LED_GREEN: Option<Pin<bank0::Gpio5, FunctionSio<SioOutput>, PullDown>> = None;
-
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    unsafe { LED_GREEN.as_mut().unwrap() }.set_high().unwrap();
-    let mut red_led_counter = unsafe { TIMER.unwrap().get_counter() };
+fn panic(info: &PanicInfo) -> ! {
+    let mut pac = unsafe { Peripherals::steal() };
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let clocks = init_clocks_and_plls(
+        EXTERN_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
+    let usb_bus = UsbBusAllocator::new(UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+    let mut serial = SerialPort::new(&usb_bus);
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .strings(&[StringDescriptors::default()
+            .manufacturer("Fake company")
+            .product("Serial port")
+            .serial_number("TEST")])
+        .unwrap()
+        .device_class(2)
+        .build();
+
+    // TODO: fix not printing here
+    _ = writeln!(serial, "{}", info);
+
+    let mut dummy_buffer = [0; 8];
     loop {
-        let current_count = unsafe { TIMER.unwrap().get_counter() };
-        if (current_count - red_led_counter).to_millis() > 1000 {
-            red_led_counter = current_count;
-            unsafe { LED_RED.as_mut().unwrap() }.toggle().unwrap();
+        if usb_dev.poll(&mut [&mut serial]) {
+            _ = serial.read(&mut dummy_buffer);
         }
     }
 }
@@ -63,10 +87,7 @@ fn main() -> ! {
     )
     .unwrap();
 
-    let _timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
-    unsafe {
-        TIMER = Some(_timer);
-    }
+    let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
     // The single-cycle I/O block controls our GPIO pins
     let sio = Sio::new(pac.SIO);
@@ -84,7 +105,7 @@ fn main() -> ! {
     let _mode1 = pins.gpio2.into_push_pull_output();
     let _mode0 = pins.gpio3.into_push_pull_output();
     let _standby = pins.gpio4.into_push_pull_output();
-    let led_green = pins.gpio5.into_push_pull_output();
+    let mut led_green = pins.gpio5.into_push_pull_output();
     let _switch_ino = pins.gpio6.into_push_pull_output();
     let _t_ready = pins.gpio7.into_push_pull_output();
     let _eeprom_scl = pins.gpio8.into_push_pull_output();
@@ -107,16 +128,16 @@ fn main() -> ! {
 
     let _rh_pwm = pins.gpio23.into_push_pull_output();
     let _tec_pwm = pins.gpio24.into_push_pull_output();
-    let led_red = pins.gpio25.into_push_pull_output();
+    let _led_red = pins.gpio25.into_push_pull_output();
     let _c_test = pins.gpio26.into_push_pull_output();
     let _dis_6 = pins.gpio27.into_push_pull_output();
     let _dis_5 = pins.gpio28.into_push_pull_output();
-    let _hv_feed = pins.gpio29.into_push_pull_output();
 
-    unsafe {
-        LED_GREEN = Some(led_green);
-        LED_RED = Some(led_red);
-    }
+    //  hal docs use floating input for adc
+    let hv_feed = pins.gpio29.into_floating_input();
+
+    let mut adc = Adc::new(pac.ADC, &mut pac.RESETS);
+    let mut adc_hv_feed = AdcPin::new(hv_feed).unwrap();
 
     let usb_bus = UsbBusAllocator::new(UsbBus::new(
         pac.USBCTRL_REGS,
@@ -140,126 +161,106 @@ fn main() -> ! {
     enum Command {
         Get,
         Set(usize),
+        GetHVFeed,
     }
+
+    /*
+        get hvfeed analog digital converter
+        0-3.3 -> 0-x (voltage divider)
+    */
 
     let mut dummy_user_controlled_value = 42;
 
-    const COMMAND_BUFFER_CAPACITY: usize = 32;
-    let mut command_buffer = [0u8; COMMAND_BUFFER_CAPACITY];
-    let mut command_buffer_len = 0;
-    let mut input_buffer = [0u8; 32];
-    let mut green_led_counter = unsafe { TIMER.unwrap().get_counter() };
+    const INPUT_BUFFER_CAPACITY: usize = 32;
+    let mut input_buffer = [0u8; INPUT_BUFFER_CAPACITY];
+    let mut input_buffer_len = 0;
+    let mut green_led_counter = timer.get_counter();
 
     loop {
         if usb_dev.poll(&mut [&mut serial]) {
-            match serial.read(&mut input_buffer) {
+            match serial.read(&mut input_buffer[input_buffer_len..]) {
                 Err(_) | Ok(0) => (),
                 Ok(count) => {
-                    for byte in input_buffer.iter().take(count) {
-                        if command_buffer_len == COMMAND_BUFFER_CAPACITY {
-                            _ = serial.write_all(&input_buffer);
-                            _ = serial.write_all(
-                                b"\r\nCommand buffer is full, clearing command buffer!\r\n",
-                            );
-                            command_buffer_len = 0;
-                            break;
-                        }
-
-                        command_buffer[command_buffer_len] = *byte;
-                        command_buffer_len += 1;
-
+                    // TODO: Potentially make all lowercase
+                    for byte in input_buffer.iter().skip(input_buffer_len).take(count) {
                         if *byte == b'\r' {
-                            _ = serial.write(b"\\r\r\n");
-                            command_buffer_len -= 1;
-                            break;
+                            _ = serial.write_all(b"\\r\r\n");
+                        } else {
+                            _ = serial.write_all(&[*byte]);
                         }
-                        _ = serial.write_all(&[*byte]);
+                    }
+                    input_buffer_len += count;
+                    if input_buffer_len == INPUT_BUFFER_CAPACITY {
+                        _ = serial.write_all(
+                            b"\r\ninput buffer is full, no commands are this long, clearing it!\r\n",
+                        );
+                        input_buffer_len = 0;
                     }
                 }
             }
         }
 
         let mut command: Option<Command> = None;
-        if command_buffer[command_buffer_len] == b'\r' {
-            match command_buffer[0..command_buffer_len] {
-                [b'G', b'e', b't'] => {
+        if input_buffer_len != 0 && input_buffer[input_buffer_len - 1] == b'\r' {
+            match input_buffer[0..input_buffer_len] {
+                // get hv feed*
+                [b'g', b'e', b't', b' ', b'h', b'v', b' ', b'f', b'e', b'e', b'd', ..] => {
+                    command = Some(Command::GetHVFeed);
+                }
+                // get*
+                [b'g', b'e', b't', ..] => {
                     command = Some(Command::Get);
                 }
-                [b'S', b'e', b't', b' ', ..] => {
+                // set *
+                [b's', b'e', b't', b' ', ..] => {
                     let mut number: String<32> = String::new();
-                    for byte in command_buffer[4..command_buffer_len].iter() {
+                    for byte in input_buffer[4..input_buffer_len - 1].iter() {
                         number.push(*byte as char).unwrap();
                     }
                     if let Ok(n) = number.parse::<usize>() {
                         command = Some(Command::Set(n));
                     } else {
-                        _ = serial.write_all(b"failed parsing number to set");
+                        _ = serial.write_all(b"failed parsing number to set\r\n");
                     }
                 }
                 _ => {
-                    _ = serial.write_all(b"unrecognized command");
-                    command_buffer_len = 0;
+                    _ = serial.write_all(b"unrecognized command\r\n");
                 }
             }
+            input_buffer_len = 0;
         }
         match command {
             Some(Command::Get) => {
-                let mut text: String<32> = String::new();
-                writeln!(&mut text, "current value: {}", dummy_user_controlled_value).unwrap();
+                write!(serial, "current value: {}\r\n", dummy_user_controlled_value).unwrap();
             }
             Some(Command::Set(n)) => {
                 dummy_user_controlled_value = n;
             }
+            Some(Command::GetHVFeed) => {
+                let adc_counts_hv_feed: u16 = adc.read(&mut adc_hv_feed).unwrap();
+                write!(serial, "adc_counts_hv_feed: {}\r\n", adc_counts_hv_feed).unwrap();
+                let counts_max: f32 = 4095.0;
+                let divided_volts_hv_feed = (adc_counts_hv_feed as f32) / counts_max * 3.3;
+                write!(
+                    serial,
+                    "divided_volts_hv_feed : {}\r\n",
+                    divided_volts_hv_feed
+                )
+                .unwrap();
+                let volts_hv_feed = divided_volts_hv_feed * (6.49f32 + 1000f32) / 6.49f32;
+                write!(serial, "volts_hv_feed : {}\r\n", volts_hv_feed).unwrap();
+            }
             None => (),
         }
 
-        let current_count = unsafe { TIMER.unwrap().get_counter() };
+        let current_count = timer.get_counter();
         if (current_count - green_led_counter).to_millis() > 1000 {
-            green_led_counter = current_count;
-            unsafe { LED_GREEN.as_mut().unwrap() }.toggle().unwrap();
+            if (current_count - green_led_counter).to_millis() > 2000 {
+                led_green.set_high().unwrap();
+                green_led_counter = current_count;
+            } else {
+                led_green.set_low().unwrap();
+            }
         }
     }
-
-    //     let mut said_hello = false;
-    //     loop {
-    //         // A welcome message at the beginning
-    //         if !said_hello && timer.get_counter().ticks() >= 2_000_000 {
-    //             said_hello = true;
-    //             let _ = serial.write(b"Hello, World!\r\n");
-
-    //            let time = timer.get_counter().ticks();
-    //            writeln!(&mut text, "Current timer ticks: {}", time).unwrap();
-    //             // the serial port is smaller than the buffers available to the USB
-    //             // peripheral. In general, the return value should be handled, so that
-    //             // bytes not transferred yet don't get lost.
-    //            let _ = serial.write(text.as_bytes());
-
-    //        // Check for new data
-    //             let mut buf = [0u8; 64];
-    //             match serial.read(&mut buf) {
-    //                 Err(_e) => {
-    //                     // Do nothing
-    //                 }
-    //                Ok(0) => {
-    //                 }
-    //                 Ok(count) => {
-    //                     // Convert to upper case
-    //                     buf.iter_mut().take(count).for_each(|b| {
-    //                         b.make_ascii_uppercase();
-    //                     });
-    //                     // Send back to the host
-    //                     let mut wr_ptr = &buf[..count];
-    //                     while !wr_ptr.is_empty() {
-    //                         match serial.write(wr_ptr) {
-    //                             Ok(len) => wr_ptr = &wr_ptr[len..],
-    //                             // On error, just drop unwritten data.
-    //                             // One possible error is Err(WouldBlock), meaning the USB
-    //                             // write buffer is full.
-    //                             Err(_) => break,
-    //                         };
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
 }
